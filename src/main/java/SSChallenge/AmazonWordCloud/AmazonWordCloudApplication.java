c2932ccd0ff401546837c650a9af9dab9d6c2de0
Federico Rossendy <fede07@users.noreply.github.com>
2024-05-27 15:43:30 -0300

    Inicio

---
+++ src/main/java/SSChallenge/AmazonWordCloud/AmazonWordCloudApplication.java
@@ -0,0 +1,13 @@
+package SSChallenge.AmazonWordCloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmazonWordCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmazonWordCloudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/SSChallenge/AmazonWordCloud/AmazonWordCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package SSChallenge.AmazonWordCloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmazonWordCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
