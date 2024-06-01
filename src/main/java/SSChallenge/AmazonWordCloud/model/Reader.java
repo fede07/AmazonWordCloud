277a4e7f6eb47062181745bb15fe0333ddb55b7a
Federico Rossendy <fede07@users.noreply.github.com>
2024-06-01 01:28:18 -0300

    Language Refactoring. Added Docs

--- src/main/java/SSChallenge/AmazonWordCloud/model/Reader.java
+++ src/main/java/SSChallenge/AmazonWordCloud/model/Reader.java
@@ -7,15 +7,17 @@ import java.io.FileNotFoundException;
 import java.util.HashSet;
 import java.util.Scanner;
 
-
 /**
  * Lector de archivos.
  */
 @Component
 public class Reader {
+
+    private static final String path = "src/main/resources/stopWords/english.txt";
+
     public static void main(String[] args) {
         try {
-            File file = new File("src/main/resources/stopWords/english.txt");
+            File file = new File(path);
             Scanner scanner = new Scanner(file);
             while (scanner.hasNextLine()) {
                 String word = scanner.nextLine();
@@ -34,7 +36,7 @@ public class Reader {
     public HashSet<String> getStopWords() {
         HashSet<String> stopWords = new HashSet<>();
         try {
-            File file = new File("src/main/resources/stopWords/english.txt");
+            File file = new File(path);
             Scanner scanner = new Scanner(file);
             while (scanner.hasNextLine()) {
                 String word = scanner.nextLine();
